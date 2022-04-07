f697593fdebd017ef581d6b09c2ee363aaf077ec
Antonio Iorio <antonioiorio94@gmail.com>
2022-04-07 21:21:50 +0200

    aggiunta prodotti sezione main

---
+++ src/components/ComicsItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="card">
+    <img :src="comic.thumb" alt="" />
+    <p>{{ comic.series }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ComicItem",
+  props: {
+    comic: Object,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.card {
+  width: calc(100% / 6);
+  img {
+    max-width: -webkit-fill-available;
+    box-shadow: 10px 5px 25px rgba(255, 255, 255, 0.627);
+  }
+  p {
+    text-align: center;
+    padding: 1rem;
+  }
+}
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,23 +1,39 @@
 <template>
   <main>
     <div>
-      <h2>-- Content goes here --</h2>
+      <div class="container">
+        <ComicsItem
+          v-for="(item, index) in Comics"
+          :key="index"
+          :comic="item"
+        />
+      </div>
     </div>
   </main>
 </template>
 
 <script>
+import ComicsItem from "@/components/ComicsItem.vue";
+
 export default {
   name: "mainComponent",
+  props: {
+    Comics: Array,
+  },
+  components: { ComicsItem },
 };
 </script>
 
 <style lang="scss" scoped>
 main {
-  background-color: #000;
+  background-color: #1c1c1c;
   div {
     padding: 50px;
     color: white;
+    .container {
+      display: flex;
+      flex-wrap: wrap;
+    }
   }
 }
 </style>
